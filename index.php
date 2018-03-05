29d9ea47c5d8da363c99ff8a7e4562b0192f2ee6
mmantle-hud <m.e.mantle@hud.ac.uk>
2018-03-05 11:23:43 +0000

    Cleaned up the instructions

--- index.php
+++ index.php
@@ -5,11 +5,10 @@
 </head>
 <body>
 <?php
-/*
+
 class ProjectManager{
 	private $builder;
 	private $joiner;
-	private $electrician;
 	function __construct($builder,$joiner)
 	{
 		$this->builder = $builder;
@@ -21,55 +20,15 @@ class ProjectManager{
 		echo "<p>".$this->builder->digFoundations()."</p>";
 		echo "<p>".$this->builder->buildWalls()."</p>";
 		echo "<p>".$this->joiner->makeStairs()."</p>";
-		// $plumber->fitSink();
-		// $electrician->fitLighting();
 	}
 	public function payContractors(){
 		echo "<p>".$this->builder->pay()."</p>";
 		echo "<p>".$this->joiner->pay()."</p>";
-		// $plumber->pay();
-		// $electrician->pay();
 	}
-	
-	// function __construct()
-	// {
-	// 	$this->contractors = [];
-	// }
-	// public function hireContractor($contractor){
-	// 	$this->contractors[]=$contractor;
-	// }
-
-}
-*/
-
-
-class ProjectManager{
-	private $contractors;
-	function __construct()
-	{
-		$this->contractors = [];
-	}
-	public function hireContractor($contractor){
-		$this->contractors[]=$contractor;
-	}
-	
-	public function buildHouse()
-	{
-		foreach($this->contractors as $contractor){
-			echo "<p>".$contractor->work()."</p>";
-		}
-	}
-	public function payContractors(){
-		foreach($this->contractors as $contractor){
-			echo "<p>".$contractor->pay()."</p>";
-		}
-	}
-	
-	
-
 }
 
-Abstract class Contractor{
+
+class Builder {
 	private $firstName;
 	private $lastName;
 	function __construct($firstName,$lastName)
@@ -81,57 +40,23 @@ Abstract class Contractor{
 	{
 		return $this->firstName." ".$this->lastName;
 	}
-	public function pay(){
-		return $this->getFullName()." just got paid.";
-	}
-	abstract function work();
-}
-
-class Builder extends Contractor{
 	public function digFoundations()
 	{
 		return $this->getFullName()." has dug some foundations.";
 	}
-	public function buildWalls()
-	{
-		return $this->getFullName()." has built the walls.";
-	}
-	public function work(){
-		return $this->buildWalls()." ".$this->digFoundations();
+	public function pay(){
+		return $this->getFullName()." just got paid.";
 	}
 }
 
-class Joiner extends Contractor{
-	public function work()
-	{
-		return $this->getFullName()." has made some stairs.";
-	}
-}
-
-class Electrician extends Contractor{
-	public function work()
-	{
-		return $this->getFullName()." has fitted the lights.";
-	}
-}
-
-class Plumber extends Contractor{
-	public function work()
-	{
-		return $this->getFullName()." has fitted a sink.";
-	}
-}
-
-
-
-
-// $builder = new Builder("Jane","Smith");
-// echo "<p>".$builder->digFoundations()."</p>"; //Jane Smith has dug some foundations.
+$builder = new Builder("Jane","Smith");
+echo "<p>".$builder->digFoundations()."</p>"; //Jane Smith has dug some foundations.
 
 /*
 Open this page in a browser. You should get the message 'Jane Smith has dug some foundations'
 Add another method to the Builder class. Call it buildWalls(). This method should return a simple string stating the builder is building walls. Uncomment the following code to check this works
 */
+
 //echo "<p>".$builder->buildWalls()."</p>"; //Jane Smith has built the walls.
 
 /*
@@ -155,23 +80,21 @@ Now uncomment the ProjectManager class. Create an instance of ProjectManager (sp
 // $projectManager->payContractors();
 
 /*
-Create another class, call this class Electrician. It will need to inherit from the Contractor class. Add a fitLighting method it should return a string stating the electrician has fitted the lights
+Create another class, call this class Electrician. It will need to inherit from the Contractor class. Add a fitLighting method. This method should return a string stating the electrician has fitted the lights.
 */
 
 // $electrician = new Electrician("Carla","Green");
-// echo "<p>".$electrician->fitLighting()."</p>"; //Carla has made some stairs.
-// echo "<p>".$electrician->pay()."</p>";
+// echo "<p>".$electrician->fitLighting()."</p>"; 
+// echo "<p>".$electrician->pay()."</p>"; 
 
 /*
 Modify the ProjectManager class so that it also uses an Electrician object
 */
 
 /*
-
-Add an abstract method to Contractor, name it work(). Implement the work() method in Builder, Joiner and Electrician classes. Again here's some example code. Depending on how you have implemented this some of your previous code e.g. $electrician->fitLighting() might now give errors. if ti does comment out these lines. 
-
-
+Add an abstract method to Contractor, name it work(). Implement the work() method in Builder, Joiner and Electrician classes. Again here's some example code. Depending on how you implement this some of your previous code e.g. $electrician->fitLighting() might now give errors. If it does comment out these lines. 
 */
+
 // $builder = new Builder("Jane","Smith");
 // $joiner = new Joiner("Imran","Iqbal");
 // $electrician = new Electrician("Carla","Green");
@@ -180,12 +103,10 @@ Add an abstract method to Contractor, name it work(). Implement the work() metho
 // echo "<p>".$electrician->work()."</p>"; //Carla Green has fitted the lights. 
 
 /*
-Modify the project manager class The problem with this is that everytime we create a new kind of Contractor we have to make changes to the ProjectManager class (by adding extra properties, parameters etc.). It would be nice if we could add new types of Contractor without changing the ProjectManager class. Have a go at the following:
-
+One problem is that everytime we create a new kind of Contractor we have to make changes to the ProjectManager class (by adding extra properties, parameters etc.). It would be nice if we could add new types of Contractor without changing the ProjectManager class. Have a go at the following:
 Remove the $builder, $joiner and $electrician properties from the ProjectManager class.
 Replace them with a single $contractors property.
-In the constructor function set $contractors to be an empty array i.e.
-$contractors = [];
+In the constructor function set $contractors to be an empty array i.e. $contractors = [];
 
 Add a hireContractor() method that will allow us to add elements (builder, joiner etc.) to the array e.g.
 
@@ -196,8 +117,8 @@ public function hireContractor($contractor){
 Modify the buildHouse and payContractors methods so that they user forEach loops call methods of Contractor objects e.g.
 
 foreach($this->contractors as $contractor){
-			echo "<p>".$contractor->work()."</p>";
-		}
+	echo "<p>".$contractor->work()."</p>";
+}
 
 Test this works by uncommenting the following code
 
@@ -214,21 +135,11 @@ Test this works by uncommenting the following code
 // $projectManager->payContractors();
 
 /*
-Finally add another child class of Contractor, name it Plumber. The work method should return a string such as plumber has fitted the sink. Modify the above code so that a plumber is also added to the project. 
+Finally add another child class of Contractor, name it Plumber. The work method should return a string such as 'plumber has fitted the sink'. Modify the above code so that a plumber is also added to the project. 
 
 IMPORTANT - Notice how we can now add a plumber and we don't have to make any changes to ProjectManager class. This is the advantage of polymorphism. 
 */
-$builder = new Builder("Jane","Smith");
-$joiner = new Joiner("Imran","Iqbal");
-$electrician = new Electrician("Carla","Green");
-$plumber = new Plumber("Clinton","Woods");
-$projectManager = new ProjectManager();
-$projectManager->hireContractor($builder);
-$projectManager->hireContractor($joiner);
-$projectManager->hireContractor($electrician);
-$projectManager->hireContractor($plumber);
-$projectManager->buildHouse();
-$projectManager->payContractors();
+
 
 
 
