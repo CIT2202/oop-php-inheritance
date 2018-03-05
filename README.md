e8d4fc3f735937c5f4d1ad6a7e83965677f29014
mmantle-hud <m.e.mantle@hud.ac.uk>
2018-03-05 11:28:00 +0000

    Fixed the markdown

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#Inheritance in OOP in PHP
+# Inheritance in OOP in PHP
 
 Open index.php in a text editor and answer the questions. 
--- index.php
+++ index.php
@@ -53,14 +53,14 @@ $builder = new Builder("Jane","Smith");
 echo "<p>".$builder->digFoundations()."</p>"; //Jane Smith has dug some foundations.
 
 /*
-Open this page in a browser. You should get the message 'Jane Smith has dug some foundations'
+1. Open this page in a browser. You should get the message 'Jane Smith has dug some foundations'
 Add another method to the Builder class. Call it buildWalls(). This method should return a simple string stating the builder is building walls. Uncomment the following code to check this works
 */
 
 //echo "<p>".$builder->buildWalls()."</p>"; //Jane Smith has built the walls.
 
 /*
-When you build a house you don't just need builder. Create a Joiner class. The Joiner class will need first name and last name properties and it will need to feature the getFullName(), and pay() methods just like in the Builder class. You will also need to add a makeStairs() method. makeStairs() should return a simple string stating the joiner has made some stairs. Uncomment the following code to check this works. 
+2. When you build a house you don't just need builder. Create a Joiner class. The Joiner class will need first name and last name properties and it will need to feature the getFullName(), and pay() methods just like in the Builder class. You will also need to add a makeStairs() method. makeStairs() should return a simple string stating the joiner has made some stairs. Uncomment the following code to check this works. 
 */
 
 // $joiner = new Joiner("Imran","Iqbal");
@@ -68,11 +68,11 @@ When you build a house you don't just need builder. Create a Joiner class. The J
 // echo "<p>".$joiner->pay()."</p>"; //Imran Iqbal just got paid.
 
 /*
-Have a look at  the Builder and Joiner classes. Have a go at creating a parent class called Contractor and have Builder and Joiner inherit from Contractor. Make Contractor Abstract (we don't want to create any Contractor instances). Think what properties and methods Joiner and Builder have in common.  All the previous statements for creating instances should still work. 
+3. Have a look at  the Builder and Joiner classes. Have a go at creating a parent class called Contractor and have Builder and Joiner inherit from Contractor. Make Contractor Abstract (we don't want to create any Contractor instances). Think what properties and methods Joiner and Builder have in common.  All the previous statements for creating instances should still work. 
 */
 
 /*
-Now uncomment the ProjectManager class. Create an instance of ProjectManager (specifying the builder and joiner as arguments when you call the constructor function). Test this works by calling the buildHouse and payContractors methods
+4. Now uncomment the ProjectManager class. Create an instance of ProjectManager (specifying the builder and joiner as arguments when you call the constructor function). Test this works by calling the buildHouse and payContractors methods
 */
 
 // $projectManager = new ProjectManager($builder, $joiner);
@@ -80,7 +80,7 @@ Now uncomment the ProjectManager class. Create an instance of ProjectManager (sp
 // $projectManager->payContractors();
 
 /*
-Create another class, call this class Electrician. It will need to inherit from the Contractor class. Add a fitLighting method. This method should return a string stating the electrician has fitted the lights.
+5. Create another class, call this class Electrician. It will need to inherit from the Contractor class. Add a fitLighting method. This method should return a string stating the electrician has fitted the lights.
 */
 
 // $electrician = new Electrician("Carla","Green");
@@ -88,11 +88,11 @@ Create another class, call this class Electrician. It will need to inherit from 
 // echo "<p>".$electrician->pay()."</p>"; 
 
 /*
-Modify the ProjectManager class so that it also uses an Electrician object
+6. Modify the ProjectManager class so that it also uses an Electrician object
 */
 
 /*
-Add an abstract method to Contractor, name it work(). Implement the work() method in Builder, Joiner and Electrician classes. Again here's some example code. Depending on how you implement this some of your previous code e.g. $electrician->fitLighting() might now give errors. If it does comment out these lines. 
+7. Add an abstract method to Contractor, name it work(). Implement the work() method in Builder, Joiner and Electrician classes. Again here's some example code. Depending on how you implement this some of your previous code e.g. $electrician->fitLighting() might now give errors. If it does comment out these lines. 
 */
 
 // $builder = new Builder("Jane","Smith");
@@ -103,7 +103,7 @@ Add an abstract method to Contractor, name it work(). Implement the work() metho
 // echo "<p>".$electrician->work()."</p>"; //Carla Green has fitted the lights. 
 
 /*
-One problem is that everytime we create a new kind of Contractor we have to make changes to the ProjectManager class (by adding extra properties, parameters etc.). It would be nice if we could add new types of Contractor without changing the ProjectManager class. Have a go at the following:
+8. One problem is that everytime we create a new kind of Contractor we have to make changes to the ProjectManager class (by adding extra properties, parameters etc.). It would be nice if we could add new types of Contractor without changing the ProjectManager class. Have a go at the following:
 Remove the $builder, $joiner and $electrician properties from the ProjectManager class.
 Replace them with a single $contractors property.
 In the constructor function set $contractors to be an empty array i.e. $contractors = [];
@@ -121,9 +121,8 @@ foreach($this->contractors as $contractor){
 }
 
 Test this works by uncommenting the following code
-
-
 */
+
 // $builder = new Builder("Jane","Smith");
 // $joiner = new Joiner("Imran","Iqbal");
 // $electrician = new Electrician("Carla","Green");
@@ -135,7 +134,7 @@ Test this works by uncommenting the following code
 // $projectManager->payContractors();
 
 /*
-Finally add another child class of Contractor, name it Plumber. The work method should return a string such as 'plumber has fitted the sink'. Modify the above code so that a plumber is also added to the project. 
+9. Add another child class of Contractor, name it Plumber. The work method should return a string such as 'plumber has fitted the sink'. Modify the above code so that a plumber is also added to the project. 
 
 IMPORTANT - Notice how we can now add a plumber and we don't have to make any changes to ProjectManager class. This is the advantage of polymorphism. 
 */
